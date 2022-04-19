832eb4f82787402f07fa4f69a17502b636b5d0d4
Ilia Gilmijarow <ilia.gilmijarow@capgemini.com>
2022-04-19 14:33:59 +0200

    another test

--- test/tests.py
+++ test/tests.py
@@ -1,15 +1,26 @@
 import unittest
 import chispa
 from pyspark.sql import SparkSession
-from pyspark.sql import utils
+from pyspark.sql import utils, dataframe
 from  src import main
+import os
 
 class dataReadTests(unittest.TestCase):
+
     def setUp(self):
-        self.session = SparkSession.builder.master("local")\
-          .appName("test_task")\
+        self.session = SparkSession.builder\
+          .master('local')\
+          .appName('test_task')\
           .getOrCreate()
+        self.sample_file_name = 'test/test_dataset.csv'
+        self.cwd = os.getcwd()
+          
     def test_fail_if_file_does_not_exist(self):
-        non_existent_file_name = 'my_bogus_file.csv'
+        non_existent_file_name = self.cwd + '/my_bogus_file.csv'
         with self.assertRaises(utils.AnalysisException):
             main.read_file(non_existent_file_name, self.session)
+            
+    def test_return_fd_if_file_exists(self):
+        my_file = self.sample_file_name
+        the_file = main.read_file(my_file, self.session)
+        self.assertIsInstance(the_file, dataframe.DataFrame)
