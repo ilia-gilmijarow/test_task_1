9fa66ba2c3cd35be92dae090437aeb4098cc1651
Ilia Gilmijarow <ilia.gilmijarow@capgemini.com>
2022-04-19 12:26:05 +0200

    add action to remove SPARK_HOME

--- test/tests.py
+++ test/tests.py
@@ -5,10 +5,12 @@ from pyspark.sql import utils
 from  src import main
 
 class dataReadTests(unittest.TestCase):
+
     def setUp(self):
         self.session = SparkSession.builder.master("local")\
           .appName("test_task")\
           .getOrCreate()
+          
     def test_fail_if_file_does_not_exist(self):
         non_existent_file_name = 'my_bogus_file.csv'
         with self.assertRaises(utils.AnalysisException):
