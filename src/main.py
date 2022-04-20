9e56dad8dd454e7679827f53e4975aee31bf07a3
Ilia Gilmijarow <ilia.gilmijarow@capgemini.com>
2022-04-20 15:39:54 +0200

    developing main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from pyspark.sql.functions import col
+#! /bin/env python3.7
 from collections.abc import Collection
+from pyspark.sql import SparkSession
+from pyspark.sql import utils, dataframe, functions
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from sys import argv
+from sys import getcwd
+import logging
+from logging.handlers import RotatingFileHandler
 
+
+#--- https://www.blog.pythonlibrary.org/2014/02/11/python-how-to-create-rotating-logs/ ---
+def create_rotating_log(path):
+    """
+    Creates a rotating log
+    """
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    # add a rotating handler
+    handler = RotatingFileHandler(path, maxBytes=20, backupCount=5)
+    logger.addHandler(handler)
+    return logger
+    
 
 def read_file(file_name, session):
     '''open input file for reading within a session'''
+    log.INFO(f'reading file {file_name}')
     return session.read.option('compression', 'none').csv(file_name)
    
    
@@ -19,10 +40,11 @@ def filter_df(df, column_name, values):
     :return: filtered data frame
     :rtype DataFrame:
     '''
+    log.INFO('filtering on f{column_name} with f{str(values)}')
     if isinstance(values, Collection):
-        filtered = df.filter(col(column_name).isin(values))
+        filtered = df.filter(functions.col(column_name).isin(values))
     else:
-        filtered = df.filter(col(column_name) == values)
+        filtered = df.filter(functions.col(column_name) == values)
     return filtered
 
 
@@ -36,7 +58,34 @@ def rename_columns(df, column_name_mapping):
     :return: data frame with new column names
     :rtype DataFrame:
     '''
-    for k, v in column_name_mapping.items():
-        df = df.withColumnRenamed(k, v)
     
+    for k, v in column_name_mapping.items():
+        log.INFO('renaming column f{k} with f{v}')
+        df = df.withColumnRenamed(k, v)
     return df
+    
+
+if __name__ == "__main__":
+    log_file = "test.log"
+    log = create_rotating_log(log_file)
+    #Check if we have all arguments and inform user otherwise
+    if len(argv) < 4:
+        msg = ['please provide 3 space-separated arguments:',
+               'the two file names and a comma-separated list of',
+               'countries to filter. If country names containspace enclose in parentheses',
+               'example:',
+               '         main.py dataset_one.csv dataset_two.csv\n']
+        print(*msg, sep='\n')
+        log.ERROR('too few parameters provided')
+        exit(1)
+    file_one, file_two, countries = argv[1:4]
+    # read the files
+    
+    # rename columns
+    
+    # drop irrelvant columns
+    
+    # join
+    
+    
+    
