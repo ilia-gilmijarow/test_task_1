7d4ff4aa9f39c53899f2e9c81f1b1943f09cc697
Ilia Gilmijarow <ilia.gilmijarow@capgemini.com>
2022-04-22 16:35:19 +0200

    more fixes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 #! /bin/env python3.7
 from collections.abc import Collection
 from pyspark.sql import SparkSession
-from pyspark.sql import utils, dataframe, functions
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import functions
 from sys import argv
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
 
-#--- https://www.blog.pythonlibrary.org/2014/02/11/python-how-to-create-rotating-logs/ ---
+# --- https://www.blog.pythonlibrary.org/2014/02/11/python-how-to-create-rotating-logs/ ---
 def create_rotating_log(path, size=2000):
     """
     Creates a rotating log
@@ -20,27 +19,27 @@ def create_rotating_log(path, size=2000):
     handler = RotatingFileHandler(path, maxBytes=size, backupCount=5)
     logger.addHandler(handler)
     return logger
-    
+
 
 def read_file(file_name, session):
-    '''open input file for reading within a session'''
+    """open input file for reading within a session"""
     log.info(f'reading file {file_name}')
     return session.read.option('header', True).csv(file_name)
-   
-   
+
+
 def filter_df(df, column_name, values):
-    '''
+    """
     filter data frame preserving values in the given column_name
     :param DataFrame df: the data frame to be filtered
     :param str column_name: the name of the column to apply the filter
-    :param collection or other values: if a collection, e.g. list is provided, 
-                                     the .isin(...) method will be called, otherwise 
+    :param collection or other values: if a collection, e.g. list is provided,
+                                     the .isin(...) method will be called, otherwise
                                      the equality operator will be used as if
                                      this parameter contains a single value, e.g. a string
     :return: filtered data frame
     :rtype DataFrame:
-    '''
-    log.info(f'filtering on fcolumn_name} with {str(values)}')
+    """
+    log.info(f'filtering on {column_name} with {str(values)}')
     if isinstance(values, Collection):
         filtered = df.filter(functions.col(column_name).isin(values))
     else:
@@ -48,18 +47,18 @@ def filter_df(df, column_name, values):
     return filtered
 
 
-def rename_columns(df, column_name_mapping):
-    '''
+def rename_columns(df, col_name_mapping):
+    """
     return a data frame with the same data, but with columns renamed according to mapping
     :param DataFrame df: the data frame
-    :param dict or another mapping column_name_mapping: mapping where keys are existing column names, 
+    :param dict or another mapping col_name_mapping: mapping where keys are existing column names,
                                                         and their values are the new column names
                                                         if there are superfluous keys in the mapping they are ignored.
     :return: data frame with new column names
     :rtype DataFrame:
-    '''
-    
-    for k, v in column_name_mapping.items():
+    """
+
+    for k, v in col_name_mapping.items():
         log.info(f'renaming column {k} with {v}')
         df = df.withColumnRenamed(k, v)
     return df
@@ -71,11 +70,11 @@ log.info('STARTED NEW RUN')
 
 if __name__ == "__main__":
 
-    #Check if we have all arguments and inform user otherwise
+    # Check if we have all arguments and inform user otherwise
     if len(argv) < 4:
         msg = ['please provide 3 space-separated arguments:',
                'the two file names and a comma-separated list of',
-               'countries to filter. If country names containspace enclose in parentheses',
+               'countries to filter. If country names contain space enclose in parentheses',
                'example:',
                '         main.py dataset_one.csv dataset_two.csv\n']
         print(*msg, sep='\n')
@@ -84,13 +83,13 @@ if __name__ == "__main__":
     file_one, file_two, countries = argv[1:4]
     countries = [x.strip() for x in countries.split(',')]
     # read the files
-    the_session = SparkSession.builder\
-      .master('local')\
-      .appName('main')\
-      .getOrCreate()
+    the_session = SparkSession.builder \
+        .master('local') \
+        .appName('main') \
+        .getOrCreate()
     df_1 = read_file(file_one, the_session)
     df_2 = read_file(file_two, the_session)
-    # drop irrelvant columns
+    # drop irrelevant columns
     log.info('dropping unneeded columns')
     df_1 = df_1.drop('first_name', 'last_name')
     df_2 = df_2.drop('cc_n')
@@ -100,12 +99,11 @@ if __name__ == "__main__":
     # filter
     df_3 = filter_df(df_3, 'country', countries)
     # rename columns
-    column_name_mapping = {'id':'client_identifier',
-                            'btc_a':'bitcoin_address',
-                            'cc_t':'credit_card_type'}
+    column_name_mapping = {'id': 'client_identifier',
+                           'btc_a': 'bitcoin_address',
+                           'cc_t': 'credit_card_type'}
     df_3 = rename_columns(df_3, column_name_mapping)
     script_path = os.path.dirname(os.path.realpath(__file__))
-    target_path = os.path.join(script_path,'..','client_data')
+    target_path = os.path.join(script_path, '..', 'client_data')
     log.info(f'saving to {target_path}')
     df_3.coalesce(1).write.mode('overwrite').csv(target_path)
-    
--- test/test_all.py
+++ test/test_all.py
@@ -2,63 +2,64 @@ import pytest
 import unittest
 from chispa.dataframe_comparer import assert_df_equality
 from pyspark.sql import SparkSession
-from pyspark.sql import utils, dataframe, functions
+from pyspark.sql import utils, dataframe
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from src import main
 
-class dataReadTests(unittest.TestCase):
+
+class DataReadTests(unittest.TestCase):
 
     def setUp(self):
-        self.session = SparkSession.builder\
-          .master('local')\
-          .appName('test_task')\
-          .getOrCreate()
+        self.session = SparkSession.builder \
+            .master('local') \
+            .appName('test_task') \
+            .getOrCreate()
         self.sample_file_name = 'test/test_dataset.csv'
-          
+
     def test_fail_if_file_does_not_exist(self):
         non_existent_file_name = '/my_bogus_file.csv'
         with self.assertRaises(utils.AnalysisException):
             main.read_file(non_existent_file_name, self.session)
-            
+
     def test_return_fd_if_file_exists(self):
         my_file = self.sample_file_name
         the_file = main.read_file(my_file, self.session)
         self.assertIsInstance(the_file, dataframe.DataFrame)
-     
-     
+
+
 @pytest.fixture()
 def session():
-    my_session = SparkSession.builder\
-      .master('local')\
-      .appName('chispa')\
-      .getOrCreate()
+    my_session = SparkSession.builder \
+        .master('local') \
+        .appName('chispa') \
+        .getOrCreate()
     return my_session
 
 
 @pytest.fixture()
 def schema():
     my_schema = StructType([
-                        StructField('id', IntegerType(), nullable=False),
-                        StructField('first_name', StringType(), nullable=False),
-                        StructField('last name', StringType(), nullable=False),
-                        ])
+        StructField('id', IntegerType(), nullable=False),
+        StructField('first_name', StringType(), nullable=False),
+        StructField('last name', StringType(), nullable=False),
+    ])
     return my_schema
 
 
 @pytest.fixture()
 def data():
     return [
-            (1, 'Hosea', 'Odonnell'),
-            (2, 'Murray', 'Weber'),
-            (3, 'Emory', 'Giles'),
-            (4, 'Devin', 'Ayala'),
-            (5, 'Rebekah', 'Rosario'),
-            (6, 'Tracy', 'Gardner'),
-            (7, 'Hosea', 'Blackwell'),
-            (8, 'Madeline', 'Black'),
-            (9, 'Jim', 'Delacruz'),
-            (10, 'Abigail', 'Giles')
-        ]
+        (1, 'Hosea', 'Odonnell'),
+        (2, 'Murray', 'Weber'),
+        (3, 'Emory', 'Giles'),
+        (4, 'Devin', 'Ayala'),
+        (5, 'Rebekah', 'Rosario'),
+        (6, 'Tracy', 'Gardner'),
+        (7, 'Hosea', 'Blackwell'),
+        (8, 'Madeline', 'Black'),
+        (9, 'Jim', 'Delacruz'),
+        (10, 'Abigail', 'Giles')
+    ]
 
 
 @pytest.fixture()
@@ -68,24 +69,24 @@ def df(session, schema, data):
 
 def test_filtering_by_collection_leaves_needed_rows(session, schema, df):
     remaining_data = [
-            (1, 'Hosea', 'Odonnell'),
-            (5, 'Rebekah', 'Rosario'),
-            (7, 'Hosea', 'Blackwell')
-            ]
-    remaining_df = session.createDataFrame(data=remaining_data, 
-                                                schema=schema)
+        (1, 'Hosea', 'Odonnell'),
+        (5, 'Rebekah', 'Rosario'),
+        (7, 'Hosea', 'Blackwell')
+    ]
+    remaining_df = session.createDataFrame(data=remaining_data,
+                                           schema=schema)
     filters = ['Hosea', 'Rebekah']
     filtered = main.filter_df(df, 'first_name', filters)
     assert_df_equality(remaining_df, filtered)
-    
-    
+
+
 def test_filtering_by_string_leaves_needed_rows(session, schema, df):
     remaining_data = [
-            (1, 'Hosea', 'Odonnell'),
-            (7, 'Hosea', 'Blackwell')
-            ]
-    remaining_df = session.createDataFrame(data=remaining_data, 
-                                                schema=schema)
+        (1, 'Hosea', 'Odonnell'),
+        (7, 'Hosea', 'Blackwell')
+    ]
+    remaining_df = session.createDataFrame(data=remaining_data,
+                                           schema=schema)
     filters = 'Hosea'
     filtered = main.filter_df(df, 'first_name', filters)
     assert_df_equality(remaining_df, filtered)
@@ -94,10 +95,10 @@ def test_filtering_by_string_leaves_needed_rows(session, schema, df):
 @pytest.fixture()
 def df2(session, data):
     my_new_schema = StructType([
-                        StructField('id', IntegerType(), nullable=False),
-                        StructField('name', StringType(), nullable=False),
-                        StructField('surname', StringType(), nullable=False),
-                        ])
+        StructField('id', IntegerType(), nullable=False),
+        StructField('name', StringType(), nullable=False),
+        StructField('surname', StringType(), nullable=False),
+    ])
     return session.createDataFrame(data=data, schema=my_new_schema)
 
 
@@ -107,8 +108,7 @@ def test_renamed_columns_are_reachable(df, df2):
     assert_df_equality(df, df2)
 
 
-def test_renaming_missing_cilumns_is_ignored(df):
+def test_renaming_missing_columns_is_ignored(df):
     column_remap = {'my_first_name': 'my name', 'my last name': 'my surname'}
     df2 = main.rename_columns(df, column_remap)
     assert_df_equality(df2, df)
-    
